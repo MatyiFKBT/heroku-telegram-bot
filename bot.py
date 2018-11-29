bf026a3e019c8576c4a61d37e2c29492cf8b6a61
MatyiFKBT <mmatyi1999@gmail.com>
2018-11-29 09:19:13 +0100

    webhook

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 import os
 import telebot
+from flask import Flask, request
 
 token = os.environ['TELEGRAM_TOKEN']
-
 bot = telebot.TeleBot(token)
+server = Flask(__name__)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -13,5 +14,19 @@ def send_welcome(message):
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
 	bot.reply_to(message, message.text)
+@server.route('/' + token, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://telebot-market.herokuapp.com/' + token)
+    return "!", 200
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    
 bot.polling()
